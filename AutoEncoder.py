08e41169c6566558c21178c53d97a9200cf9c179
feng <fengqingsong90@gmail.com>
2018-06-22 19:44:59 +0800

    add activation function

--- AutoEncoder.py
+++ AutoEncoder.py
@@ -25,43 +25,42 @@ class autoencoder(object):
     def _build_net(self):
 
         # check whether number of encode layers is valid
-        if len(self.nodes) < 2:
+        if len(self._nodes) < 2:
             raise ValueError('At least 2 encode layers should be specified.')
 
-        with tf.variable_scope(name) as scope:
+        with tf.variable_scope(self._name) as scope:
 
             # define palceholder
-            self.X = tf.placeholder(tf.float32, [None, self.nodes[0]])
+            self.X = tf.placeholder(tf.float32, [None, self._nodes[0]])
             self.learning_rate = tf.placeholder(tf.float32)
             self.keep_prob = tf.placeholder(tf.float32)
 
             layer = self.X
 
             # define encode layer
-            for i, node in enumerate(nodes[:-2]):
-                layer = self._dense_layer(shape=nodes[i, i+2], inputs=layer, 
+            for i, node in enumerate(self._nodes[:-2]):
+                layer = self._dense_layer(shape=self._nodes[i, i+2], inputs=layer, 
                                           layer_name='encoder_' + str(i+1))
-                layer = tf.nn.relu(layer)
+                layer = self._activation(layer)
                 layer = tf.nn.dropout(layer, keep_prob=self.keep_prob)
 
             # define last encode layer
-            layer = self._dense_layer(shape=nodes[len(nodes)-2:len(nodes)], inputs=layer, 
-                                      layer_name='encoder_' + str(len(nodes)-1))
+            layer = self._dense_layer(shape=self._nodes[len(self._nodes)-2:len(self._nodes)], inputs=layer, 
+                                      layer_name='encoder_' + str(len(self._nodes)-1))
             
             # get hidden layer feature
             self.hidden_feature = layer
 
-            layer = tf.nn.relu(layer)
-            layer = tf.nn.dropout(layer, keep_prob=self.keep_prob)
+            layer = self._activation(layer)
 
             # define decode layer
-            for i, node in enumerate(nodes[::-1][:-2]):
-                layer = self._dense_layer(shape=nodes[::-1][i, i+2], inputs=layer,
+            for i, node in enumerate(self._nodes[::-1][:-2]):
+                layer = self._dense_layer(shape=self._nodes[::-1][i, i+2], inputs=layer,
                                           layer_name='decoder_' + str(i+1))
-                layer = tf.nn.relu(layer)
+                layer = self._activation(layer)
 
-            self.outputs = self._dense_layer(shape=nodes[::-1][len(nodes)-2:len(nodes)],
-                                             inputs=layer, layer_name='decoder_' + str(len(node)-1))
+            self.outputs = self._dense_layer(shape=self._nodes[::-1][len(self._nodes)-2:len(self._nodes)],
+                                             inputs=layer, layer_name='decoder_' + str(len(self._nodes)-1))
 
         # define loss function and optimizer
         self.loss = self._loss_function(self.outputs, self.X)
@@ -73,6 +72,10 @@ class autoencoder(object):
             W = tf.get_variable(layer_name + '_W', shape=shape, initializer=tf.contrib.layers.xavier_initializer())
             b = tf.Variable(tf.random_normal([shape[-1]]))
         return tf.add(tf.matmul(inputs, W), b)
+
+    # define activation function
+    def _activation(self, inputs):
+        return tf.nn.relu(inputs)
 
     # define loss function
     def _loss_function(self, predict, labels):
@@ -95,7 +98,7 @@ class autoencoder(object):
                                    self.learning_rate: learning_rate, 
                                    self.keep_prob: keep_prob})
                 avg_loss += l / total_batch
-            print "Epoch ", "%04d" %(epoch+1), "loss = ", "{:.9f}".format(avg_loss)
+            print("Epoch ", "%04d" %(epoch+1), "loss = ", "{:.9f}".format(avg_loss))
 
             losses.append(avg_loss)
         return losses
